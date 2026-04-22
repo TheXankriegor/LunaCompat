417306afdf674cba6e6ceaebdce709d81a72df5e
TheXankriegor <Kiliancohrs@web.de>
2026-04-22 09:31:22 +0200

    feat: add base KC sync

---
+++ LunaCompatCommon/Messages/ModMessages/KerbalColoniesMessages.cs
@@ -0,0 +1,30 @@
+﻿namespace LunaCompatCommon.Messages.ModMessages
+{
+    public class KerbalColoniesRequestColoniesMessage : IModMessage
+    {
+    }
+
+    public abstract class KerbalColoniesColonyMessage : IModMessage
+    {
+        #region Properties
+
+        public string Body { get; set; }
+
+        public string ColonyName { get; set; }
+
+        #endregion
+    }
+
+    public class KerbalColoniesDeleteColonyMessage : KerbalColoniesColonyMessage
+    {
+    }
+
+    public class KerbalColoniesChangeColonyMessage : KerbalColoniesColonyMessage
+    {
+        #region Properties
+
+        public string Content { get; set; }
+
+        #endregion
+    }
+}
